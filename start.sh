b275d881323ab89bf041a1f38aadf207234c19d6
seph <seph@directionless.org>
2025-06-01 15:25:09 -0400

    closer

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+HOSTS_ALLOW=${HOSTS_ALLOW:-"127."}
+SERVER_STRING=${SERVER_STRING:-"Samba Server"}
+MEDIA_MOUNT=${MEDIA_MOUNT:-"/mnt/media"}
+
+sed -e "s|@HOSTS_ALLOW@|${HOSTS_ALLOW}|g" \
+    -e "s|@SERVER_STRING@|${SERVER_STRING}|g" \
+    -e "s|@MEDIA_MOUNT@|${MEDIA_MOUNT}|g" \
+    /etc/samba/smb.conf.template /etc/samba/smb.conf
+
+
+# Display some data
+echo "ip configuration:"
+ip a
+
+echo
+echo "Starting Samba"
+smbd -F --no-process-group </dev/null
